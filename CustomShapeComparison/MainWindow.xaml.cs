7edb0aa9863a4373eb635244b4bab3de480e24e7
Toby <toby.lawrance@gmail.com>
2021-11-26 23:04:52 +0000

    Initial comparer

---
+++ CustomShapeComparison/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CustomShapeComparison;
+using StrokeSimiliarity;
+
+namespace CombinedGestures
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly IStrokeComparer comparer;
+
+        private IEnumerable<Point> square;
+        private IEnumerable<Point> star;
+        private IEnumerable<Point> triangle;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            comparer = new PointSimilarityComparer();
+            
+            CreateShapes();
+        }
+
+        private void CreateShapes()
+        {
+            square = new[] { new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(0, 1), new Point(0, 0) };
+            //star = new[] { new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(0, 1), new Point(0, 0) };
+            triangle = new[] { new Point(0, 0), new Point(1, 0), new Point(0.5, 1), new Point(0, 0)};
+        }
+
+        public void CreateTextBlock(string message, double posX, double posY)
+        {
+            var textBox = new TextBlock
+            {
+                Background = Brushes.Beige,
+                Foreground = Brushes.Black,
+                Text = message
+            };
+            DrawingCanvas.Children.Add(SetElementTopLeftPositionOnInkCanvas(textBox, posX, posY));
+        }
+
+        private void DrawingCanvas_OnStylusInAirMove(object sender, StylusEventArgs e)
+        {
+            
+        }
+
+        private void DrawingCanvas_OnStylusMove(object sender, StylusEventArgs e)
+        {
+            
+        }
+
+        private T SetElementCenterPositionOnInkCanvas<T>(T element,double x, double y)
+            where T : FrameworkElement
+        {
+            InkCanvas.SetTop(element, y - (element.Height/2.0));
+            InkCanvas.SetLeft(element, x - (element.Width/2.0));
+            return element;
+        }
+        
+        private T SetElementTopLeftPositionOnInkCanvas<T>(T element,double x, double y)
+            where T : FrameworkElement
+        {
+            InkCanvas.SetTop(element, y);
+            InkCanvas.SetLeft(element, x);
+            return element;
+        }
+
+        private void DrawingCanvas_OnStrokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e)
+        {
+            var score = comparer.Compare(scaledSquare,e.Stroke.StylusPoints.Select(sp => new Point(sp.X, sp.Y)));
+            
+            CreateTextBlock($"Score: {score.score}", e.Stroke.StylusPoints.Last().X,e.Stroke.StylusPoints.Last().Y);
+
+            foreach (var (p1,p2) in score.sampled.Zip(score.sampled.Skip(1), (p1,p2) => (p1,p2)))
+            {
+                var l = new Line
+                {
+                    X1 = p1.Item1.X + e.Stroke.StylusPoints.First().X,
+                    X2 = p2.Item1.X + e.Stroke.StylusPoints.First().X,
+                    Y1 = p1.Item1.Y + e.Stroke.StylusPoints.First().Y,
+                    Y2 = p2.Item1.Y + e.Stroke.StylusPoints.First().Y,
+                    Stroke = p1.distance switch
+                    {
+                        <= 5 => Brushes.Green,
+                        <= 25 => Brushes.Orange,
+                        <= 50 => Brushes.Yellow,
+                        _ => Brushes.Red
+                    }
+                };
+
+                DrawingCanvas.Children.Add(l);
+            }
+            DrawingCanvas.Strokes.Remove(e.Stroke);
+        }
+
+        private Point[] scaledSquare;
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            scaledSquare = square.Select(p => new Point(p.X * 200, p.Y * 200)).ToArray();
+            var pl = new Polyline
+            {
+                Points = new PointCollection(scaledSquare),
+                Stroke = Brushes.LightGray,
+                StrokeThickness = 5.0,
+                Fill = Brushes.Transparent,
+                FillRule = FillRule.Nonzero
+            };
+
+            pl = SetElementTopLeftPositionOnInkCanvas(pl, DrawingCanvas.ActualWidth / 2.0, DrawingCanvas.ActualHeight / 2.0);
+            DrawingCanvas.Children.Add(pl);
+        }
+    }
+}
---
+++ CustomShapeComparison/PointSimilarityComparer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Windows;
+using StrokeSimiliarity;
+
+namespace CustomShapeComparison
+{
+    public class PointSimilarityComparer : IStrokeComparer
+    {
+        private int samplesToTake = 1000;
+
+        public PointSimilarityComparer()
+        {
+            
+        }
+
+        public PointSimilarityComparer(int samples)
+        {
+            samplesToTake = samples;
+        }
+        
+        public (double score,IEnumerable<(Point,double distance)> sampled) Compare(IEnumerable<Point> baseStroke, IEnumerable<Point> compareStroke)
+        {
+            var baseSampled = TransformToSampled(baseStroke.Distinct().ToArray()).ToArray();
+            var compareSampled = TransformToSampled(compareStroke.Distinct().ToArray()).ToArray();
+
+            baseSampled = baseSampled.Select(p => new Point(p.X - baseSampled.First().X, p.Y - baseSampled.First().Y)).ToArray();
+            compareSampled = compareSampled.Select(p => new Point(p.X - compareSampled.First().X, p.Y - compareSampled.First().Y)).ToArray();
+
+            var sampleMeasured = baseSampled.Zip(compareSampled, (p1, p2) => (p2,p1.GetDistanceTo(p2))).ToArray();
+
+            return (sampleMeasured.Average(p => p.Item2),sampleMeasured);
+        }
+
+        private IEnumerable<Point> TransformToSampled(Point[] points)
+        {
+            var totalLength = GetPathLength(points);
+            var distancePerSample = totalLength / samplesToTake;
+
+            var distances = Enumerable.Range(0, samplesToTake).Select(i => i * distancePerSample).ToArray();
+
+            return distances.Select(d => SampleLineAtDistance(points, d)).ToArray();
+        }
+
+        private static Point SampleLineAtDistance(Point[] polyLine, double distance)
+        {
+            var windowed = GetWindowedPoints(polyLine).ToArray();
+
+            var cumulativeDistance = 0.0;
+            foreach (var (p1,p2) in windowed)
+            {
+                var dist = p1.GetDistanceTo(p2);
+                if (cumulativeDistance + dist >= distance)
+                {
+                    var distanceAlong = distance - cumulativeDistance;
+                    return GetPointXDistanceAlongLine(p1, p2, distanceAlong);
+                }
+
+                cumulativeDistance += dist;
+            }
+
+            return polyLine.Last();
+        }
+
+        private static Point GetPointXDistanceAlongLine(Point p1, Point p2, double distance) => InterpolateBetweenTwoPoints(p1, p2, Math.Clamp(distance / p1.GetDistanceTo(p2),0.0,1.0));
+
+        private static Point InterpolateBetweenTwoPoints(Point p1, Point p2, double amount) => amount switch
+        {
+            <= 0.0 => p1,
+            >= 1.0 => p2,
+            _ => new Point((p2.X - p1.X) * amount + p1.X, (p2.Y - p1.Y) * amount + p1.Y)
+        };
+
+        private static IEnumerable<(Point p1, Point p2)> GetWindowedPoints(Point[] points) =>
+            points.Zip(points.Skip(1), (p, p1) => (p, p1));
+
+        private static double GetPathLength(Point[] points) =>
+            GetWindowedPoints(points).Select(pair => pair.Item1.GetDistanceTo(pair.Item2)).Sum();
+    }
+}
